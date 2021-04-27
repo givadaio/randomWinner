d234a636acd11bd2ce39d0b90a98ad1acf49d717
givadaio <81925846+givadaio@users.noreply.github.com>
2021-04-26 21:29:47 -0400

    Create get_stake.py
    Tool used to get stake for an epoch

---
+++ get_stake.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import sys
+
+TOKEN = "BLOCKFROST TOKEN"
+
+headers = {"project_id": TOKEN}
+
+def getDelegators(pool_id):
+
+    url = f"https://cardano-mainnet.blockfrost.io/api/v0/pools/{pool_id}/delegators"
+
+    resp = requests.get(url, headers=headers)
+    return json.loads(resp.text)
+
+def getStakeAmount(accounts, pool_id, epoch):
+    amounts = {}
+
+    for account in accounts:
+        address = account['address']
+        url = f"https://cardano-mainnet.blockfrost.io/api/v0/accounts/{address}/history"
+
+        resp = requests.get(url, headers=headers)
+        history = json.loads(resp.text)
+
+        for snapshot in history:
+            if snapshot["active_epoch"] == epoch:
+                if snapshot["pool_id"] == pool_id:
+                    amounts[address] = snapshot["amount"]
+
+    return amounts
+
+if __name__ == "__main__":
+    pool_id = sys.argv[1]
+    epoch = int(sys.argv[2])
+    accounts = getDelegators(pool_id)
+    
+    amounts = getStakeAmount(accounts, pool_id, epoch)
+
+    print(json.dumps(amounts, sort_keys=True, indent=4))
